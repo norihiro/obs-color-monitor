adfc7002d79e73aea032bc3f05f83c365b3b50d6
Norihiro Kamae <norihiro@nagater.net>
2021-05-06 01:51:07 +0900

    ci(win): link w32-pthread

--- ci/windows/prepare-windows.cmd
+++ ci/windows/prepare-windows.cmd
@@ -2,9 +2,21 @@ mkdir build32
 mkdir build64
 
 cd build32
-cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_VERSION=10.0 -DQTDIR="%QTDIR32%" -DLibObs_DIR="%OBSPath%\build32\libobs" -DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" -DLIBOBS_LIB="%OBSPath%\build32\libobs\%build_config%\obs.lib" -DOBS_FRONTEND_LIB="%OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib" ..
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_VERSION=10.0 ^
+-DQTDIR="%QTDIR32%" ^
+-DLibObs_DIR="%OBSPath%\build32\libobs" ^
+-DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" ^
+-DLIBOBS_LIB="%OBSPath%\build32\libobs\%build_config%\obs.lib" ^
+-DOBS_FRONTEND_LIB="%OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib;%OBSPath%\build32\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
+..
 cd ..\build64
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_SYSTEM_VERSION=10.0 -DQTDIR="%QTDIR64%" -DLibObs_DIR="%OBSPath%\build64\libobs" -DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" -DLIBOBS_LIB="%OBSPath%\build64\libobs\%build_config%\obs.lib" -DOBS_FRONTEND_LIB="%OBSPath%\build64\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib" ..
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_SYSTEM_VERSION=10.0 ^
+-DQTDIR="%QTDIR64%" ^
+-DLibObs_DIR="%OBSPath%\build64\libobs" ^
+-DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" ^
+-DLIBOBS_LIB="%OBSPath%\build64\libobs\%build_config%\obs.lib" ^
+-DOBS_FRONTEND_LIB="%OBSPath%\build64\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib;%OBSPath%\build64\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
+..
 
 REM Import the generated includes to get the plugin's name
 call "%~dp0..\ci_includes.generated.cmd"
