d8fd3c888a641b343373bad3c29a5b6844e65436
Norihiro Kamae <norihiro@nagater.net>
2021-05-22 15:58:35 +0900

    ci: enable profiler

--- ci/linux/build-ubuntu.sh
+++ ci/linux/build-ubuntu.sh
@@ -2,5 +2,5 @@
 set -ex
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -D ENABLE_PROFILE=ON ..
 make -j4
--- ci/windows/prepare-windows.cmd
+++ ci/windows/prepare-windows.cmd
@@ -8,6 +8,7 @@ cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_VERSION=10.0 ^
 -DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" ^
 -DLIBOBS_LIB="%OBSPath%\build32\libobs\%build_config%\obs.lib" ^
 -DOBS_FRONTEND_LIB="%OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib;%OBSPath%\build32\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
+-D ENABLE_PROFILE=ON ^
 ..
 cd ..\build64
 cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_SYSTEM_VERSION=10.0 ^
@@ -16,6 +17,7 @@ cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_SYSTEM_VERSION=10.0 ^
 -DLIBOBS_INCLUDE_DIR="%OBSPath%\libobs" ^
 -DLIBOBS_LIB="%OBSPath%\build64\libobs\%build_config%\obs.lib" ^
 -DOBS_FRONTEND_LIB="%OBSPath%\build64\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib;%OBSPath%\build64\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
+-D ENABLE_PROFILE=ON ^
 ..
 
 REM Import the generated includes to get the plugin's name
