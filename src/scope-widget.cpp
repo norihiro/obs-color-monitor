1c766b2e51d3afa526da5e162231aa7e90cf13e2
Norihiro Kamae <norihiro@nagater.net>
2021-06-26 00:23:13 +0900

    dock: add properties action in menu

--- src/scope-widget.cpp
+++ src/scope-widget.cpp
@@ -1,6 +1,7 @@
 #include <obs-module.h>
 #include <obs.h>
 #include <obs-frontend-api.h>
+#include <util/threading.h>
 #include <string>
 #include <algorithm>
 #include <QMenu>
@@ -8,15 +9,23 @@
 #include <QMouseEvent>
 #include "plugin-macros.generated.h"
 #include "scope-widget.hpp"
+#include "scope-widget-properties.hpp"
 #include "obsgui-helper.hpp"
 
-#define N_SRC 3
+#define N_SRC SCOPE_WIDGET_N_SRC
+
+static const char *id_list[N_SRC] = {
+	"vectorscope_source",
+	"waveform_source",
+	"histogram_source",
+};
 
 struct scope_widget_s
 {
 	obs_display_t *disp;
 	obs_source_t *src[N_SRC];
 	volatile uint32_t src_shown;
+	pthread_mutex_t mutex;
 };
 
 static obs_source_t *create_scope_source(const char *id)
@@ -35,15 +44,15 @@ static void draw(void *param, uint32_t cx, uint32_t cy)
 {
 	auto *data = (struct scope_widget_s*)param;
 
+	if (pthread_mutex_trylock(&data->mutex))
+		return;
+
 	for (int i=0; i<N_SRC; i++) if (!data->src[i]) {
-		static const char *id_list[N_SRC] = {
-			"vectorscope_source",
-			"waveform_source",
-			"histogram_source",
-		};
 		data->src[i] = create_scope_source(id_list[i]);
-		if (data->src[i])
+		if (data->src[i]) {
+			pthread_mutex_unlock(&data->mutex);
 			return; // not to take too much time
+		}
 	}
 
 	int n_src = 0;
@@ -73,6 +82,8 @@ static void draw(void *param, uint32_t cx, uint32_t cy)
 		y0 += h;
 		k ++;
 	}
+
+	pthread_mutex_unlock(&data->mutex);
 }
 
 ScopeWidget::ScopeWidget(QWidget *parent)
@@ -86,6 +97,7 @@ ScopeWidget::ScopeWidget(QWidget *parent)
 	setAttribute(Qt::WA_NativeWindow);
 
 	data = (struct scope_widget_s*)bzalloc(sizeof(struct scope_widget_s));
+	pthread_mutex_init(&data->mutex, NULL);
 	data->src_shown = (1<<N_SRC)-1;
 }
 
@@ -95,9 +107,14 @@ ScopeWidget::~ScopeWidget()
 		obs_display_destroy(data->disp);
 		data->disp = NULL;
 
+		pthread_mutex_lock(&data->mutex);
 		for (int i=0; i<N_SRC; i++) if (data->src[i]) {
 			obs_source_release(data->src[i]);
+			data->src[i] = NULL;
 		}
+		pthread_mutex_unlock(&data->mutex);
+
+		pthread_mutex_destroy(&data->mutex);
 	}
 	bfree(data); data = NULL;
 }
@@ -185,9 +202,76 @@ void ScopeWidget::mousePressEvent(QMouseEvent *event)
 			popup.addAction(act);
 		}
 
+		act = new QAction(obs_module_text("Properties..."), this);
+		connect(act, &QAction::triggered, this, &ScopeWidget::createProperties);
+		popup.addAction(act);
+
 		popup.exec(QCursor::pos());
 		return;
 	}
 
 	QWidget::mousePressEvent(event);
+}
+
+void ScopeWidget::createProperties()
+{
+	if (properties) {
+		if (!properties->close())
+			return;
+	}
+	properties = new ScopeWidgetProperties(this, data->src);
+	properties->Init();
+	properties->setAttribute(Qt::WA_DeleteOnClose, true);
+}
+
+void ScopeWidget::default_properties(obs_data_t *props)
+{
+	for (int i=0; i<N_SRC; i++) {
+		char s[32]; snprintf(s, sizeof(s), "%s-shown", id_list[i]); s[sizeof(s)-1]=0;
+		obs_data_set_default_bool(props, s, true);
+	}
+}
+
+void ScopeWidget::save_properties(obs_data_t *props)
+{
+	pthread_mutex_lock(&data->mutex);
+	const auto src_shown = data->src_shown;
+	for (int i=0; i<N_SRC; i++) {
+		char s[32];
+		snprintf(s, sizeof(s), "%s-shown", id_list[i]); s[sizeof(s)-1]=0;
+		obs_data_set_bool(props, s, !!(src_shown & (1<<i)));
+
+		if (data->src[i]) {
+			snprintf(s, sizeof(s), "%s-prop", id_list[i]); s[sizeof(s)-1]=0;
+			obs_data_t *prop = obs_data_create();
+			if (prop) {
+				obs_source_update(data->src[i], prop);
+				obs_data_set_obj(props, s, prop);
+				obs_data_release(prop);
+			}
+		}
+	}
+	pthread_mutex_unlock(&data->mutex);
+}
+
+void ScopeWidget::load_properties(obs_data_t *props)
+{
+	pthread_mutex_lock(&data->mutex);
+	data->src_shown = 0;
+	for (int i=0; i<N_SRC; i++) {
+		char s[32];
+		snprintf(s, sizeof(s), "%s-shown", id_list[i]); s[sizeof(s)-1]=0;
+		if (obs_data_get_bool(props, s))
+			data->src_shown |= 1<<i;
+
+		if (!data->src[i])
+			data->src[i] = create_scope_source(id_list[i]);
+		snprintf(s, sizeof(s), "%s-prop", id_list[i]); s[sizeof(s)-1]=0;
+		obs_data_t *prop = obs_data_get_obj(props, s);
+		if (prop) {
+			obs_source_update(data->src[i], prop);
+			obs_data_release(prop);
+		}
+	}
+	pthread_mutex_unlock(&data->mutex);
 }
--- src/scope-widget.hpp
+++ src/scope-widget.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <obs.h>
 #include <QWidget>
+
+#define SCOPE_WIDGET_N_SRC 3
 
 class ScopeWidget : public QWidget {
 	Q_OBJECT
 
 	struct scope_widget_s *data;
+	class ScopeWidgetProperties *properties;
 
 	void CreateDisplay();
 	void resizeEvent(QResizeEvent *event) override;
@@ -13,8 +17,16 @@ class ScopeWidget : public QWidget {
 	void closeEvent(QCloseEvent *event) override;
 	void mousePressEvent(QMouseEvent *event) override;
 
+public slots:
+	void createProperties();
+
 public:
 	ScopeWidget(QWidget *parent);
 	~ScopeWidget();
+	static void default_properties(obs_data_t*);
+	void save_properties(obs_data_t*);
+	void load_properties(obs_data_t*);
 	void setShown(bool shown);
+
+	friend class ScopeWidgetProperties;
 };
