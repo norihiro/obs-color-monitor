12aa75a156131229530072b404fdb6f6e8e41ed8
Norihiro Kamae <norihiro@nagater.net>
2021-06-26 16:36:28 +0900

    dock: Fix an issue properties are not saved

--- src/scope-widget.cpp
+++ src/scope-widget.cpp
@@ -244,9 +244,8 @@ void ScopeWidget::save_properties(obs_data_t *props)
 
 		if (data->src[i]) {
 			snprintf(s, sizeof(s), "%s-prop", id_list[i]); s[sizeof(s)-1]=0;
-			obs_data_t *prop = obs_data_create();
+			obs_data_t *prop = obs_source_get_settings(data->src[i]);
 			if (prop) {
-				obs_source_update(data->src[i], prop);
 				obs_data_set_obj(props, s, prop);
 				obs_data_release(prop);
 			}
