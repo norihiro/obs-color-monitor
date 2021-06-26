9fa80e546430c598c219a0ec1f2effd68a1b4f57
Norihiro Kamae <norihiro@nagater.net>
2021-06-26 17:09:57 +0900

    ci(linux): Fix libdir for Ubuntu

--- ci/linux/build-ubuntu.sh
+++ ci/linux/build-ubuntu.sh
@@ -2,6 +2,9 @@
 set -ex
 
 cp LICENSE data/LICENSE-$PLUGIN_NAME
+
+sed -i 's;${CMAKE_INSTALL_FULL_LIBDIR};/usr/lib;' CMakeLists.txt
+
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
