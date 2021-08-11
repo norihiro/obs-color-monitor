130238ab811bd48ca6f4c1f0553f136239c8492a
Norihiro Kamae <norihiro@nagater.net>
2021-08-11 22:32:48 +0900

    dock: Fix titile cannot be set on Windows
    There was an incorrect conversion from QString to UTF-8.

--- src/scope-dock-new-dialog.cpp
+++ src/scope-dock-new-dialog.cpp
@@ -48,7 +48,6 @@ ScopeDockNewDialog::~ScopeDockNewDialog()
 
 void ScopeDockNewDialog::accept()
 {
-	const char *name = editTitle->text().toStdString().c_str();
 	const char *srcName = NULL;
 	obs_data_t *props = obs_data_create();
 	obs_data_t *roi_prop = obs_data_create();
@@ -60,7 +59,7 @@ void ScopeDockNewDialog::accept()
 	obs_data_set_obj(props, "colormonitor_roi-prop", roi_prop);
 	ScopeWidget::default_properties(props);
 
-	scope_dock_add(name, props);
+	scope_dock_add(editTitle->text().toUtf8().constData(), props);
 
 	obs_data_release(roi_prop);
 	obs_data_release(props);
