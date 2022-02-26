363e0be0abc87e73c2128a97fc1833887e3d6947
Norihiro Kamae <norihiro@nagater.net>
2022-02-26 11:24:41 +0900

    ci/linux: Fix version of obs-frontend-api.h

--- ci/linux/install-dependencies-ubuntu.sh
+++ ci/linux/install-dependencies-ubuntu.sh
@@ -15,6 +15,7 @@ sudo apt-get install -y \
 	qtbase5-private-dev
 
 # Dirty hack
-sudo wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/25.0.0/UI/obs-frontend-api/obs-frontend-api.h
+OBS_VER="$(dpkg -s obs-studio | awk '$1=="Version:"{print gensub(/^([0-9]*\.[0-9]*)\..*/, "\\1.0", "g", $2)}')"
+sudo wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/${OBS_VER}/UI/obs-frontend-api/obs-frontend-api.h
 
 sudo ldconfig
