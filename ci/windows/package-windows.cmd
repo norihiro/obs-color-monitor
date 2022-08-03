6ff222e4dcf48b93aa8df10b3d337318b2cdf4e5
Norihiro Kamae <norihiro@nagater.net>
2022-08-03 21:51:15 +0900

    CI: Set different name for each artifact

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -10,10 +10,10 @@ del package-version.txt
 copy ..\LICENSE          ..\release\data\obs-plugins\%PluginName%\LICENCE-%PluginName%.txt
 
 REM Package ZIP archive
-7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
+7z a "%PluginName%-%PackageVersion%-obs%1-Windows.zip" "..\release\*"
 
 REM Build installer
-iscc ..\installer\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-Windows-Installer"
+iscc ..\installer\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-obs%1-Windows-Installer"
 
-certutil.exe -hashfile "%PluginName%-%PackageVersion%-Windows.zip" SHA1
-certutil.exe -hashfile "%PluginName%-%PackageVersion%-Windows-Installer.exe" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows.zip" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-Installer.exe" SHA1
