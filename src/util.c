41000cc3a8fc4916230b2a3d27e815108130df50
Norihiro Kamae <norihiro@nagater.net>
2024-11-08 12:30:20 +0900

    Fix log message not showing effect file name if the file was not found

--- src/util.c
+++ src/util.c
@@ -7,7 +7,7 @@ gs_effect_t *create_effect_from_module_file(const char *basename)
 	char *f = obs_module_file(basename);
 	gs_effect_t *effect = gs_effect_create_from_file(f, NULL);
 	if (!effect)
-		blog(LOG_ERROR, "Cannot load '%s'", f);
+		blog(LOG_ERROR, "Cannot load '%s' '%s'", basename, f);
 	bfree(f);
 	return effect;
 }
